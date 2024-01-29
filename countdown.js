88a80e9415039904cd5a3c31f556a45184d7be45
Revanth kumar S <98330771+revanth-004@users.noreply.github.com>
2024-01-30 00:59:17 +0530

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,23 @@
+const targetDate = new Date("March 12, 2024 09:00:00").getTime();
+
+const countdownInterval = setInterval(() => {
+  const now = new Date().getTime();
+
+  const timeDifference = targetDate - now;
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  console.log(days, hours, minutes, seconds);
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+
+  if (timeDifference < 0) {
+    clearInterval(countdownInterval);
+  }
+}, 1000);
